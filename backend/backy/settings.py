b69d40c4e70cd6ce89d5d88b9e1f74d508971f84
Aleksi Vesanto <avjves@gmail.com>
2021-04-22 00:54:35 +0300

    Remove atomic requests. TBF, the requests can't be fully atomic, as any 3rd party receiver won't rollback anyways.

--- backend/backy/settings.py
+++ backend/backy/settings.py
@@ -97,7 +97,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        'ATOMIC_REQUESTS': True,
+        'ATOMIC_REQUESTS': False,
     }
 }
 
