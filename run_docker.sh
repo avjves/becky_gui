daefc31fdc7d35e31391d6a1688707d46f23245f
Aleksi Vesanto <avjves@gmail.com>
2021-06-01 23:06:08 +0300

    Added s3cmd to Dockerfile

--- run_docker.sh
+++ run_docker.sh
@@ -1,4 +1,4 @@
 service nginx restart
 cd /becky/backend/
-uwsgi --http :6701 --module becky.wsgi
+uwsgi --http :6701 --module becky.wsgi --processes 4 --threads 2
 #python3 manage.py runserver 0.0.0.0:6701
